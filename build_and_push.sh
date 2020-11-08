064a9b3564fe27ce581f4b5a8e3bd99931ab2146
ds-muzalevskiy <74115150+ds-muzalevskiy@users.noreply.github.com>
2020-11-08 17:33:07 +0100

    build

---
+++ build_and_push.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+image=$1
+
+if [ "$image" == "" ]
+then
+    echo "Usage: $0 <image-name>"
+    exit 1
+fi
+
+chmod +x decision_trees/train
+chmod +x decision_trees/serve
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+if [ $? -ne 0 ]
+then
+    exit 255
+fi
+
+
+region=$(aws configure get region)
+region=${region:-eu-west-1}
+
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+
+aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${image}" > /dev/null
+fi
+
+$(aws ecr get-login --region ${region} --no-include-email)
+
+
+docker build  -t ${image} .
+docker tag ${image} ${fullname}
+
+docker push ${fullname}
